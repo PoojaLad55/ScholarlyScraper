6558199c0ab696110eb1b27248565059ec7aaeb7
PoojaLad55 <PoojaLad55@gmail.com>
2024-09-11 16:17:24 -0400

    updated chrome driver

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def setup_browser():
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-    service = Service('C:/Users/pooja/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe')
+    service = Service('C:/Users/pooja/Downloads/chromedriver-win64/chromedriver.exe')
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     return driver
